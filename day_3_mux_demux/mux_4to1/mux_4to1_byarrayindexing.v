521a40e4ab61fc7c40d08e0ecb14374cd15fb15f
Harish Murugan <harishmurugan8503@gmail.com>
2025-10-24 11:35:22 +0000

    mux design by array  indexing method

---
+++ day_3_mux_demux/mux_4to1/mux_4to1_byarrayindexing.v
@@ -0,0 +1,11 @@
+module mux_4to1_arrayindex (input [3:0]a,input [1:0]sel,output y);
+
+assign y=a[sel];       //combining input lines into single array and 
+		       //select the ouput based on 2 bit select line by index:
+		       
+		       
+		 
+
+endmodule
+
+
