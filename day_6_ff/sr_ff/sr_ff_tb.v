98ab97f30a7b8486e31175c8a131d297c0ee5498
Harish Murugan <harishmurugan8503@gmail.com>
2025-10-24 04:45:04 +0000

    latch

--- day_6_ff/sr_ff/sr_ff_tb.v
+++ day_6_ff/sr_ff/sr_ff_tb.v
@@ -17,7 +17,9 @@ end
 //stimulus generation
 initial begin
 	$monitor("time=%0t|clk=%b|rst=%b|s=%b|r=%b|q=%b",$time,clk,rst,s,r,q);
-	
+	$dumpfile("sr_ff.vcd");
+	$dumpvars();
+
 	s=1;r=1;rst=1;#10
 	s=0;r=1;rst=0;#10
 	s=0;r=0;#10
