e3f875cd430080681c103c45c299438994002f66
Harish <harishmurugan8503@gmail.com>
2026-01-26 23:29:21 +0530

    Add parameterized priority encoder module
    Implement parameterized priority encoder logic.

--- day_4_encoder_decoder/parameterized_p_encoder/p_encoder.v
+++ day_4_encoder_decoder/parameterized_p_encoder/p_encoder.v
@@ -1,1 +1,24 @@
+// parameterized priority encoder;
+
+module p_encoder #(parameter in_w)(input [in_w-1:0] in,output reg [($clog2(in_w))-1:0] out );
+  
+  always_comb begin
+    //casez(in)
+    
+    out=0;
+    //valid=|in;
+  
+    for(int i=0;i<=in_w;i++)begin
+      if(in[i]==1)begin
+        out=i;
+        break;//break
+      end
+    end
+      
+      
+    //endcase
+  end
+endmodule
+      
+      
 
