4bc70445db5037cdb9ec539ccaa1d7694c3acbae
Harish <harishmurugan8503@gmail.com>
2026-02-17 12:33:14 +0530

    Update gen_random_num.v

--- verilog_test/gen_random_num.v
+++ verilog_test/gen_random_num.v
@@ -9,7 +9,11 @@ reg signed [7:0] signed_num;
 initial begin
 	$display ("random value between -100 to 100");
 	repeat(50)begin
-		signed_num=((($random % 201 + 201) % 201)-100);
+		
+		signed_num=$random%101;//give between (-100 to +100)
+		
+	//signed_num=((($random % 201 + 201) % 201)-100);
+		
 		$display ("$time=%0t |randomvalue= %d|",$time,signed_num,);
 		#5;
 	end
